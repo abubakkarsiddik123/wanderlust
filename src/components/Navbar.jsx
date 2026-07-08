e4e60fd92d7b449d74915692c818f14c8e48cf73
abubakkarsiddik123 <abu089604@gmail.com>
2026-07-08 13:34:45 +0600

    added add-destinations

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='flex justify-between items-center p-4 shadow bg-white'>
+            <ul className='flex gap-3'>
+                <li><Link href={'/'}>Home</Link> </li>
+                <li><Link href={'/destinations'}>Destinations</Link> </li>
+                <li><Link href={'/my-bookings'}>My Bookings</Link> </li>
+                <li><Link href={'/add-destination'}>Add Destination</Link> </li>
+            </ul>
+            <div className="">
+                <Image 
+                src={'/assets/Wanderlast.png'}
+                alt='wanderlust logo'
+                width={150}
+                height={150}
+                />
+            </div>
+            <ul className='flex gap-3'>
+                <li><Link href={'/profile'}>Profile</Link> </li>
+                <li><Link href={'/login'}>Login</Link> </li>
+                <li><Link href={'/signup'}>Sign Up</Link> </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
